cac81b6e1178c709b85aaac867a22ce712511cba
unknown <thoyla@QFINW007.it.local>
2016-01-21 14:28:37 +0200

    search and return ok

--- js/contents.js
+++ js/contents.js
@@ -1,4 +1,25 @@
 $( document ).ready(function() {
+    /*
+    var states = ['Alabama', 'Alaska', 'Arizona', 'Arkansas', 'California',
+  'Colorado', 'Connecticut', 'Delaware', 'Florida', 'Georgia', 'Hawaii',
+  'Idaho', 'Illinois', 'Indiana', 'Iowa', 'Kansas', 'Kentucky', 'Louisiana',
+  'Maine', 'Maryland', 'Massachusetts', 'Michigan', 'Minnesota',
+  'Mississippi', 'Missouri', 'Montana', 'Nebraska', 'Nevada', 'New Hampshire',
+  'New Jersey', 'New Mexico', 'New York', 'North Carolina', 'North Dakota',
+  'Ohio', 'Oklahoma', 'Oregon', 'Pennsylvania', 'Rhode Island',
+  'South Carolina', 'South Dakota', 'Tennessee', 'Texas', 'Utah', 'Vermont',
+  'Virginia', 'Washington', 'West Virginia', 'Wisconsin', 'Wyoming'
+];
+
+*/
+    
+    var ruokaAineet = ['Hunaja', 'Kaurahiutale', 'Kauraryyni', 'Ohrajauho', 'Ruisjauho', 'vehnäjauho', 'riisi', 'Sokeri', 'Voi', 'Öljy', 'Suola', 'Pippuri', 'Bulgur', 'Maizena-suuruste', 'Bearnaisekastikejauhe', 'Ahven', 'Anjovis', 'Artisokka', 'Appelsiini', 'Aprikoosi', 'Ananas', 'Avokado', 'seitan', 'Peruna', 'tofu', 'Kaali', 'Kyssäkaali', 'Keräkaali', 'Kiinankaali', 'Jauheliha'
+    ];
+    
+    var receipes = 
+        '{"Resepti": [{"nimi":"Hedelmä-jälkiruoka"}, {"ainekset":["Appelsiini", "Aprikoosi", "Ananas"]}],[{"nimi":"Jauhelihakeitto"}, {"ainekset":["Jauheliha", "Peruna", "Suola"]}]}';
+    //var arr = JSON.parse(pets);
+    
 var substringMatcher = function(strs) {
   return function findMatches(q, cb) {
     var matches, substringRegex;
@@ -21,16 +42,8 @@ var substringMatcher = function(strs) {
   };
 };
 
-var states = ['Alabama', 'Alaska', 'Arizona', 'Arkansas', 'California',
-  'Colorado', 'Connecticut', 'Delaware', 'Florida', 'Georgia', 'Hawaii',
-  'Idaho', 'Illinois', 'Indiana', 'Iowa', 'Kansas', 'Kentucky', 'Louisiana',
-  'Maine', 'Maryland', 'Massachusetts', 'Michigan', 'Minnesota',
-  'Mississippi', 'Missouri', 'Montana', 'Nebraska', 'Nevada', 'New Hampshire',
-  'New Jersey', 'New Mexico', 'New York', 'North Carolina', 'North Dakota',
-  'Ohio', 'Oklahoma', 'Oregon', 'Pennsylvania', 'Rhode Island',
-  'South Carolina', 'South Dakota', 'Tennessee', 'Texas', 'Utah', 'Vermont',
-  'Virginia', 'Washington', 'West Virginia', 'Wisconsin', 'Wyoming'
-];
+
+
 
 $('#the-basics .typeahead').typeahead({
   hint: true,
@@ -38,7 +51,30 @@ $('#the-basics .typeahead').typeahead({
   minLength: 1
 },
 {
-  name: 'states',
-  source: substringMatcher(states)
+  name: 'ruokaAineet',
+  source: substringMatcher(ruokaAineet)
 });
+    
+    $(".btn").click(function(){
+    var addedstate = $('#datainput').val();
+    var ingredientId = $.inArray(addedstate, ruokaAineet);
+        
+    if (ingredientId != -1) {
+        $( "#content" ).append(ruokaAineet[ingredientId]);
+    }
+        
+    /**
+    Eli nokkimisjärjestys on tämä:
+        - lisää jokainen lisätty state ingredients[]-listaan
+        tee lista ingredientsien pohjalta (#content)-diviin pilsseinä
+        
+        Hae raaka-aineet 
+    */
+        
+    
+    
+    
+    
+});
+    
 });
